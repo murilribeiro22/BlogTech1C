d8526f608100abd38edba40b6f38703d798f28cd
murilribeiro22 <murilo.greinert@escola.pr.gov.br>
2026-08-03 11:51:09 -0300

    a

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const botao = document.querySelector("button");
 botao.addEventListener("click", botaoClicado);
+const botao = document.querySelector("button");
+botao.addEventListener("click", botaoClicado);
+
+function botaoClicado() {
+console.log("fui clicado");
+}
+
+function botaoClicado() {
+let texto = botao.querySelector("span");
+texto.textContent++;
+}
